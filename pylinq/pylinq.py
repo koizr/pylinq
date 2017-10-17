95d1371c18489b1b978d01840fa982f5e786d0ef
koizumin <koizumi.ryo.dev@gmail.com>
2017-10-18 00:07:13 +0900

    change type hints

--- pylinq/pylinq.py
+++ pylinq/pylinq.py
@@ -1,4 +1,4 @@
-import typing
+from typing import Iterable, Callable
 
 
 class Enumerable(object):
@@ -6,31 +6,31 @@ class Enumerable(object):
     this class is lazy iterable like LINQ.
     """
 
-    def __init__(self, values):
+    def __init__(self, values: Iterable):
         self.values = (x for x in values)
 
     def __iter__(self):
         return self.values
 
     @classmethod
-    def from_(cls, values):
+    def from_(cls, values: Iterable) -> 'Enumerable':
         return cls(values)
 
     @classmethod
-    def range(cls, *args):
+    def range(cls, *args) -> 'Enumerable':
         """
         組み込みの range() と同じ挙動で Enumerable を生成する
         """
         return cls(range(*args))
 
     @classmethod
-    def empty(cls):
+    def empty(cls) -> 'Enumerable':
         """
         空の Enumerable を返す
         """
         return cls([])
 
-    def where(self, predicate: typing.Callable):
+    def where(self, predicate: Callable) -> 'Enumerable':
         """
         受け取った関数を適用してフィルタリングした結果を返す
         """
